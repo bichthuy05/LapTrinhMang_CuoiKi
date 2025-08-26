4f907d4ae7e7b0b4037ac60cc42a7d2c2fa03b33
Mỹ Hoàng <myyhoangg205@gmail.com>
2025-08-27 00:32:22 +0700

    Delete

--- server/services/group_service.py
+++
@@ -1,8 +0,0 @@
-# Minimal stub for demo
-class GroupService:
-    def create_group(self, payload: dict):
-        name = payload.get("group_name")
-        members = payload.get("members") or []
-        if not name or not members:
-            return False, {"message": "group_name/members required"}
-        return True, {"group": {"name": name, "members": members}}
--- server/utils/helpers.py
+++
@@ -1,7 +0,0 @@
-import uuid, datetime
-
-def generate_id() -> str:
-    return str(uuid.uuid4())
-
-def now_iso() -> str:
-    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
